9f7a5448d58327824fac4b002c127b9015bd6a13
Douglas Miner <curr3nas@gmail.com>
2020-03-05 11:57:09 -0600

    wired the event listeners, working on the functions to set state

--- src/Card.js
+++ src/Card.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './Card.css';
 
 export default function Card(props) {
+  
+  console.log(props)
+
   return (
     <div className='Card'>
       <button
+        id={props.id}
         type='button'
+        onClick={e => props.onDelete(e.id, e.id)}
       >
         delete
       </button>
